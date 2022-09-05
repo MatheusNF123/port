f00e91680a72ff1dda8d8dac86888b2cf06d72f8
Matheus <matheus_cs.nf@hotmail.com>
2022-09-05 12:34:00 -0300

    menu mobila

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 // import {Link} from 'react-scroll'
+import { GoThreeBars } from "react-icons/go";
 import './Header.css'
 
 function Header() {
+const [menuAtivo, setMenuAtivo] = useState(false)
+  const toggleMenu = (e) => {
+    if(e.type === 'touchstart') e.preventDefault()
+    setMenuAtivo(!menuAtivo)
+  }
 
   return(
-    <header>     
-      <section className="div-content-header">
+    <header className="header">      
         <h3>My-Port</h3>
-        <nav id="navbar-example2" className="navbar bg-bg-dark ">
+        <nav id="navbar-example2" className={`navbar ${menuAtivo ? 'menuAtivo' : ''}`}>
+          <button className="menu-mobile" onClick={toggleMenu} onTouchStart={toggleMenu}>
+            <span className="spanMenu">Menu</span>
+            <GoThreeBars className="iconeHambur"/>
+          </button>
         <ul className="listaNavegacao">
           <li> <a href="#home">Home</a> </li>
           <li> <a href="#sobre">Sobre</a> </li>
@@ -16,7 +25,7 @@ function Header() {
           <li> <a href="#contate">Contate-me</a> </li>
         </ul>
         </nav>
-      </section>
+     
     </header>
   )
 }
